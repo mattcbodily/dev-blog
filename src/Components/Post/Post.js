31895358a85d4e62145d45e2e23cd6baa9c0724f
mattcbodily <mattcbodily@gmail.com>
2020-09-14 16:06:29 -0600

    fixing errors

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -7,18 +7,18 @@ export default props => {
     useEffect(() => {
         const selectedPost = posts.find(el => el.id === +props.match.params.id);
         setPost(selectedPost);
-    }, [])
+    }, [props.match.params.id])
 
     return (
         <section>
             <h1>{post.title}</h1>
             <img src={post.image} alt={post.title}/>
-            {post.content?.map(content => {
+            {post.content?.map((content, i) => {
                 for(let key in content){
                     if(key.includes('image')){
-                        return <img src={content[key]} alt='Blog Image'/>
-                    } else {
-                        return <p>{content[key]}</p>
+                        return <img key={i} src={content[key]} alt={content.alt}/>
+                    } else if(key.includes('paragraph')){
+                        return <p key={i}>{content[key]}</p>
                     }
                 }
             })}
