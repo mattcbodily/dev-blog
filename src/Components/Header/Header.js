d1c082a3ff151cb3fe008a7cb173cc57ffc5beb6
mattcbodily <mattcbodily@gmail.com>
2020-09-15 22:36:34 -0600

    added link back to home from posts

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Header.css';
 
 export default props => (
     <header className='main-header'>
-        <h2>Dev Blog</h2>
-        <p>About</p>
+        <Link className='link' to='/'><h2>Dev Blog</h2></Link>
+        <nav className='nav-links'>
+            <p>About</p>
+            <p>Contact</p>
+        </nav>
     </header>
 )
